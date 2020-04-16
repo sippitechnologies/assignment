08e87c08066e3c60c7c2275f466ba6323870a9b2
Sippitechnologies <sippitechnologies@gmail.com>
2020-04-16 22:41:51 +0530

    Temperature Converter app solution

--- TempartureConverter/app/src/main/java/com/sippitechnologies/tempartureconverter/MainActivity.kt
+++ TempartureConverter/app/src/main/java/com/sippitechnologies/tempartureconverter/MainActivity.kt
@@ -2,11 +2,26 @@ package com.sippitechnologies.tempartureconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Formula Fahrenheit to celsius= (temperature-32) *5/9
+        //Formula Celsius to Fahrenheit (temperature*9/5)+32
+
+        button.setOnClickListener{
+            val result = when(radioGroup.checkedRadioButtonId)
+            {
+                R.id.radioButton2-> (editText3.text.toString().toDouble()-32) *5/9
+                else-> (editText3.text.toString().toDouble()*9/5)+32
+            }
+            textView2.text=result.toString()
+        }
+
+
     }
 }
